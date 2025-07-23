6f8a1817c7f7a0fcb2d1786439ee190257181bc3
Samuel Marin <s.marin.alzate2007@gmail.com>
2025-07-23 10:09:44 -0500

    Primeras pruebas clase HorseTest.java

--- src/test/java/HorseTest.java
+++ src/test/java/HorseTest.java
@@ -1,17 +1,62 @@
-import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 
 public class HorseTest {
-    @Nested
-    public class horseTest{
-        @Test
-        public void nameNotFound(){
-            IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> new Horse(null, 0.0));
-            assertEquals("Name cannot be null.", exception.getMessage());
+    private Horse horse;
+
+    @Test
+    public void horseNameIsNull(){
+        try{
+            horse = new Horse(null, 9, 8);
+        }catch (IllegalArgumentException e){
+            assertEquals("Name cannot be null.", e.getMessage());
         }
 
+    }
+
+    @Test
+    public void horseNameIsBlank(){
+        try{
+            horse = new Horse("", 9, 8);
+        }catch (IllegalArgumentException e){
+            assertEquals("Name cannot be blank.", e.getMessage());
+        }
+
+    }
+
+    @Test
+    public void horseSpeedIsNegative(){
+        try{
+            horse = new Horse("x", -9, 8);
+        }catch (IllegalArgumentException e){
+            assertEquals("Speed cannot be negative.", e.getMessage());
+        }
+
+    }
+
+    @Test
+    public void horseDistanceIsNegative(){
+        try{
+            horse = new Horse("x", 10, -2);
+        }catch (IllegalArgumentException e){
+            assertEquals("Distance cannot be negative.", e.getMessage());
+        }
+
+    }
+
+    @Test
+    public void getterTest(){
+        String name = "name";
+        double speed = 9;
+        double distance = 8;
+        horse = new Horse(name, speed, distance);
+
+        assertAll("validate getter variables match, variables from the constructor. ",
+                () -> assertEquals(name, horse.getName()),
+                () -> assertEquals(speed, horse.getSpeed()),
+                () -> assertEquals(distance, horse.getDistance())
+        );
 
     }
 }
